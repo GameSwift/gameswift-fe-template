c2a9bdbd0edec8691fb7a1d1b792e8bbe0573c40
radekskrabucha <rskrabucha13@gmail.com>
2023-10-17 12:07:47 +0200

    feat(tailwind-config): updated tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,20 +14,22 @@ const config = {
       normal: '300',
       bold: '500'
     },
-    fontSize: {
-      xs: '0.75rem', // 12px
-      sm: '0.875rem', // 14px
-      base: '1rem', // 16px
-      lg: '1.125rem', // 18px
-      xl: '1.25rem', // 20px
-      '2xl': '1.5rem', // 24px
-      '3xl': '2.25rem', // 36px
-      '4xl': '3rem', // 48px
-      '5xl': '5rem' // 80px
-    },
     extend: {
+      fontSize: {
+        xs: '0.75rem', // 12px
+        sm: '0.875rem', // 14px
+        base: '1rem', // 16px
+        lg: '1.125rem', // 18px
+        xl: '1.25rem', // 20px
+        '2xl': '1.5rem', // 24px
+        '3xl': '2rem', // 32px
+        '4xl': '2.25rem', // 36px
+        '5xl': '3rem', // 48px
+        '6xl': '5rem' // 80px
+      },
       screens: {
-        xs: '480px'
+        xs: '480px',
+        xxs: '360px'
       },
       colors: {
         primary: {
@@ -42,7 +44,8 @@ const config = {
         },
         background: {
           DEFAULT: '#1F2128',
-          500: '#1F2128'
+          500: '#1F2128',
+          400: '#16171C'
         },
         'background-secondary': {
           DEFAULT: '#26282E',
